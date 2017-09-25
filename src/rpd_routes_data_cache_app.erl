529781c30c4663dfd28bae28d9163fe5a67d00fe
AShirko <antonshirko@frtk.ru>
2017-09-25 15:33:06 +0300

    fixed data_cache

--- src/rpd_routes_data_cache_app.erl
+++ src/rpd_routes_data_cache_app.erl
@@ -10,7 +10,7 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    rpd_routes_data_cache:start_link().
+    rpd_routes_data_cache_sup:start_link().
 
 stop(_State) ->
     ok.
