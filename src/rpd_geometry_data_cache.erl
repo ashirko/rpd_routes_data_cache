e37fbef8c537c3ed0b7950693cee211e0f9ece0d
AShirko <antonshirko@frtk.ru>
2017-09-26 11:39:12 +0300

    fixed data_cache

--- src/rpd_geometry_data_cache.erl
+++ src/rpd_geometry_data_cache.erl
@@ -97,7 +97,7 @@ load_geometry(GeomTableId,Trips)->
   lists:foreach(fun({_,TripId})->
     case rnis_data_route_geometry_loader:load_geometry_for_route(TripId) of
       {ok, Geom}->
-        lager:info("geometry data for ~p : ~p", [Trips, Geom]),
+%%        lager:info("geometry data for ~p : ~p", [Trips, Geom]),
         true = ets:insert(GeomTableId, {TripId, Geom});
       {error, Reason}->
         lager:error("error while loading geometry data for ~p : ~p", [TripId, Reason])
